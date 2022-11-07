50fad38f2061576865ebb13d15025d334defaee8
ziclude <cindyzickziemi@gmail.com>
2022-11-06 18:27:54 -0800

    fixing

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,7 +1,7 @@
 # fixes Apache error 500 by fixing it
 
 exec { 'fix typo':
-onlyif  => 'test -e /var/www/html/wp-settings.php',
-command => "sed -i 's/phpp/php/' /var/www/html/wp-settings.php",
-path	=> '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+  onlyif  => 'test -e /var/www/html/wp-settings.php',
+  command => "sed -i 's/phpp/php/' /var/www/html/wp-settings.php",
+  path	  => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
 }
